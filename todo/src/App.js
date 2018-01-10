816451730d5489e1c6ded2a8a0ada964b11d4e67
asad <asadkarim33@gmail.com>
2018-01-09 17:12:59 -0800

    todo app with removing button

---
+++ todo/src/App.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import * as firebase from 'firebase';
+import FlatButton from 'material-ui/FlatButton';
+import './App.css';
+
+
+const ref = firebase.database().ref();
+
+class App extends Component {
+  constructor(){
+    super()
+    this.state={
+      todo : '',
+      allTodos:[]
+    }
+  }
+
+
+
+  componentDidMount() {
+    const self = this;
+    // ref.child("newtodo").on("value", function(wayn) {
+       ref.on("value", function(snapshot) {
+
+
+        const data = snapshot.val();
+      if(data){
+        let allTodos = [];
+        for(let key in data){
+          const obj = {
+            name: data[key].name,
+            key: key
+        }
+          allTodos.push(obj)
+        }
+        self.setState({allTodos:allTodos})
+  
+      }
+    })
+  }
+
+
+
+
+
+
+  onChange(event){
+    this.setState ({todo: event.target.value})
+  }
+
+save(){
+  ref.push({name : this.state.todo})
+}
+
+ delete(data, event){
+     console.log(data);
+  ref.child(data.key).remove();
+
+}
+
+
+  render() {
+    return (
+      <div className="App">
+                 
+        <input type="text" value={this.state.todo} onChange={this.onChange.bind(this)} /> 
+        <button onClick={this.save.bind(this)}>Add Todo</button>
+      
+      <ul>        
+           {this.state.allTodos  &&  this.state.allTodos.map((data, index)  => {
+            return <li key={index}>{data.name}<span style={{margin:"500px"}}></span>  <FlatButton  primary={true}  onClick={this.delete.bind(this, data)}>X</FlatButton></li>
+            })}
+          </ul> 
+      
+
+      
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ todo/src/config.js
@@ -0,0 +1,11 @@
+import * as firebase from 'firebase';
+
+ var config = {
+    apiKey: "AIzaSyD4j10TnltCpFkI_A3Go5MVJWhdvB-9aKk",
+    authDomain: "todo-3fc62.firebaseapp.com",
+    databaseURL: "https://todo-3fc62.firebaseio.com",
+    projectId: "todo-3fc62",
+    storageBucket: "",
+    messagingSenderId: "36270966645"
+  };
+  firebase.initializeApp(config);
