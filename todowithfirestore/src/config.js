e613b83341b21a2bf1b81d2ba0111b0a0808a6be
Asad karim <asadkarim33@gmail.com>
2018-01-11 00:54:18 -0800

    learning firestore

---
+++ todowithfirestore/src/config.js
@@ -0,0 +1,11 @@
+import * as firebase from 'firebase';
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAMAPJzzBIxHUlhTAKyuO3oA0JBoQHfxLs",
+    authDomain: "private-chat-app-9f0f1.firebaseapp.com",
+    databaseURL: "https://private-chat-app-9f0f1.firebaseio.com",
+    projectId: "private-chat-app-9f0f1",
+    storageBucket: "private-chat-app-9f0f1.appspot.com",
+    messagingSenderId: "646867446326"
+  };
+  firebase.initializeApp(config);
