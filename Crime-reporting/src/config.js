cb031a29611cfcc36732dfd78add38b1369295a4
asad <asadkarim33@gmail.com>
2018-01-13 19:26:11 -0800

    sign in / sign up and main page is completed

---
+++ Crime-reporting/src/config.js
@@ -0,0 +1,12 @@
+import * as  firebase from 'firebase';
+
+
+var config = {
+    apiKey: "AIzaSyCR4IHUoObUAYut6tfGERb7Ra2yR2djFpM",
+    authDomain: "crime-reporting-97b00.firebaseapp.com",
+    databaseURL: "https://crime-reporting-97b00.firebaseio.com",
+    projectId: "crime-reporting-97b00",
+    storageBucket: "",
+    messagingSenderId: "932926503180"
+  };
+  firebase.initializeApp(config);
